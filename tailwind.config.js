c84b35b13450d0ed40ac74c35e32d50854e17566
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-21 11:36:49 +0200

    Added taiwlind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};
