da688447386768398f9db4eb37419cd44f63f69f
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-21 12:03:10 +0200

    Updated UI

--- src/lib/components/cards/SignupCard.svelte
+++ src/lib/components/cards/SignupCard.svelte
@@ -1,10 +1,4 @@
-<script>
-  export let margin = "mx-auto";
-</script>
-
-<div
-  class=" place-self-center h-full card w-96 bg-gray-900 shadow-xl mb-5 lg:mb-0 lg:{margin}"
->
+<div class="h-full card w-96 bg-gray-900 shadow-xl mb-5">
   <div class="card-body">
     <slot />
   </div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -34,98 +34,103 @@
   <div class="container mx-auto my-10">
     <form on:submit={submit}>
       <div class="grid lg:grid-cols-2 gap-4">
-        <SignupCard margin="ml-auto">
-          <div class="mb-4">
-            <label for="email" class="card-title mb-2">Email</label>
-            <FormInput
-              required={true}
-              type="email"
-              id="email"
-              placeholder="someone@domain.com"
-              bind:value={email}
-            />
-          </div>
-          <div class="mb-4">
-            <label for="username" class="card-title mb-2">Username</label>
-            <FormInput
-              required={true}
-              type="text"
-              id="username"
-              placeholder="Choose a username"
-              bind:value={username}
-              max="50"
-            />
-            <div class="mt-2 font-light text-sm">
-              Must use latin alphabet, be less than 50 characters, and unique.
+        <div class="flex justify-center lg:justify-end">
+          <SignupCard>
+            <div class="mb-4">
+              <label for="email" class="card-title mb-2">Email</label>
+              <FormInput
+                required={true}
+                type="email"
+                id="email"
+                placeholder="someone@domain.com"
+                bind:value={email}
+              />
             </div>
-          </div>
-          <div class="mb-4">
-            <label for="displayname" class="card-title mb-2">Display Name</label
-            >
-            <FormInput
-              required={true}
-              type="text"
-              id="displayname"
-              placeholder="Choose a display name"
-              bind:value={displayName}
-              max="50"
-            />
-            <div class="mt-2 font-light text-sm">
-              Must be less than 50 characters.
+            <div class="mb-4">
+              <label for="username" class="card-title mb-2">Username</label>
+              <FormInput
+                required={true}
+                type="text"
+                id="username"
+                placeholder="Choose a username"
+                bind:value={username}
+                max="50"
+              />
+              <div class="mt-2 font-light text-sm">
+                Must use latin alphabet, be less than 50 characters, and unique.
+              </div>
             </div>
-          </div>
-          <div class="mb-4">
-            <label for="dob" class="card-title mb-2">Birthday</label>
-            <FormInput
-              required={true}
-              type="date"
-              id="dob"
-              placeholder="Birthday"
-              bind:value={dob}
-              max={today}
-            />
-            <div class="mt-2 font-light text-sm">
-              Your birthday is private by default, and can be changed later.
+            <div class="mb-4">
+              <label for="displayname" class="card-title mb-2"
+                >Display Name</label
+              >
+              <FormInput
+                required={true}
+                type="text"
+                id="displayname"
+                placeholder="Choose a display name"
+                bind:value={displayName}
+                max="50"
+              />
+              <div class="mt-2 font-light text-sm">
+                Must be less than 50 characters.
+              </div>
             </div>
-          </div>
-        </SignupCard>
-        <SignupCard margin="mr-auto">
-          <div class="mb-4">
-            <label for="password" class="card-title mb-2">Password</label>
-            <FormInput
-              required={true}
-              type="password"
-              id="password"
-              bind:value={password}
-              min="8"
-              max="50"
-            />
-            <div class="mt-2 font-light text-sm">
-              Must be atleast 8 characters long.
+            <div class="mb-4">
+              <label for="dob" class="card-title mb-2">Birthday</label>
+              <FormInput
+                required={true}
+                type="date"
+                id="dob"
+                placeholder="Birthday"
+                bind:value={dob}
+                max={today}
+              />
+              <div class="mt-2 font-light text-sm">
+                Your birthday is private by default, and can be changed later.
+              </div>
             </div>
-          </div>
-          <div class="mb-4">
-            <label for="confirmpassword" class="card-title mb-2"
-              >Confirm Password
-            </label>
-            <FormInput
-              error={!validpass && confirmPassword.length > 0}
-              required={true}
-              type="password"
-              id="confirmpassword"
-              bind:value={confirmPassword}
-              min="8"
-              max="50"
-            />
-          </div>
-          <div class="card-actions">
-            <button
-              disabled={inProgress}
-              type="submit"
-              class="btn btn-primary text-lg w-full">Create Account</button
-            >
-          </div>
-        </SignupCard>
+          </SignupCard>
+        </div>
+        <div class="flex justify-center lg:justify-start">
+          <SignupCard>
+            <div class="mb-4">
+              <label for="password" class="card-title mb-2">Password</label>
+              <FormInput
+                required={true}
+                type="password"
+                id="password"
+                bind:value={password}
+                min="8"
+                max="50"
+              />
+              <div class="mt-2 font-light text-sm">
+                Must be atleast 8 characters long.
+              </div>
+            </div>
+            <div class="mb-4">
+              <label for="confirmpassword" class="card-title mb-2"
+                >Confirm Password
+              </label>
+              <FormInput
+                error={!validpass && confirmPassword.length > 0}
+                required={true}
+                type="password"
+                id="confirmpassword"
+                bind:value={confirmPassword}
+                min="8"
+                max="50"
+              />
+            </div>
+            <div class="card-actions">
+              <button
+                disabled={inProgress}
+                type="submit"
+                class="btn btn-primary text-lg w-full">Create Account</button
+              >
+            </div>
+          </SignupCard>
+        </div>
       </div>
     </form>
   </div>
