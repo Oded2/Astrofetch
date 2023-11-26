a5f1dbbc388797a14269318177805e227b4efd22
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-26 23:20:58 +0200

    Added drawer component

---
+++ src/lib/components/Drawer.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let title = "";
+  export let id = "drawer";
+</script>
+
+<div class="drawer">
+  <input {id} type="checkbox" class="drawer-toggle" checked />
+  <div class="drawer-side">
+    <label for={id} aria-label="close sidebar" class="drawer-overlay"></label>
+    <div class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+      {#if title.length > 0}
+        <div class="mb-6">
+          <h1 class="font-bold text-2xl">{title}</h1>
+        </div>
+      {/if}
+      <slot />
+    </div>
+  </div>
+</div>
