9a98fbc939b3f2b243adee49080de9c655ac8d48
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-27 17:30:06 +0200

    Updated formcard

--- src/lib/components/FormCard.svelte
+++ src/lib/components/FormCard.svelte
@@ -1,5 +1,14 @@
+<script>
+  export let title = "";
+</script>
+
 <div class="h-full card w-96 bg-gray-900 shadow-xl mb-5">
   <div class="card-body">
+    {#if title.length > 0}
+      <div class="mb-4">
+        <h1 class="font-bold text-xl">{title}</h1>
+      </div>
+    {/if}
     <slot />
   </div>
 </div>
