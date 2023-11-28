3786a50d107d994b012edd22f43c459cbc61f22b
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-29 00:09:11 +0200

    Profile page automatically redirects

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -1,14 +1,6 @@
 import hrefs from "$lib/hrefs.json";
-import { supabase } from "$lib/supabaseClient.js";
-import { redirect, error } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
 
-export async function load({ url }) {
-  const userId = url.searchParams.get("user_id");
-  if (!userId) throw redirect(301, hrefs.home);
-  const { data, error: supabaseError } = await supabase
-    .from("profiles")
-    .select("username")
-    .eq("user_id", userId);
-  if (supabaseError) throw error(400, "An error has occured");
-  throw redirect(301, hrefs.profile.replace("slug", data[0].username));
+export async function load() {
+  throw redirect(301, hrefs.home);
 }
