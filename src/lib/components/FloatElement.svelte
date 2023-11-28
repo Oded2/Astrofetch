666f4f56981b1ae930f54a249cf7037c3311d469
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-28 19:02:39 +0200

    Added more components

---
+++ src/lib/components/FloatElement.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let visible = true;
+</script>
+
+{#if visible}
+  <div class="m-3 print:invisible z-50 bottom-0 right-0 absolute">
+    <slot />
+  </div>
+{/if}
---
+++ src/lib/components/FullscreenModal.svelte
@@ -0,0 +1,21 @@
+<script>
+  import FloatElement from "./FloatElement.svelte";
+  import { createEventDispatcher } from "svelte";
+  export let showFullScreen = true;
+  const dispatch = createEventDispatcher();
+</script>
+
+{#if showFullScreen}
+  <div class="z-10 min-h-screen w-screen absolute top-0 bg-gray-700 h-full">
+    <slot />
+  </div>
+  <FloatElement>
+    <button on:click={dispatch("exit")} class="btn btn-neutral">Exit</button>
+  </FloatElement>
+{/if}
+
+<style>
+  :global(body) {
+    overflow: hidden;
+  }
+</style>
