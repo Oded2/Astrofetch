d88de1e775c1d17e4aaacea4d5722f62cb3b0664
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-29 16:10:02 +0200

    Fixed major bug

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -1,6 +1,14 @@
 import hrefs from "$lib/hrefs.json";
-import { redirect } from "@sveltejs/kit";
+import { supabase } from "$lib/supabaseClient.js";
+import { redirect, error } from "@sveltejs/kit";
 
-export async function load() {
-  throw redirect(301, hrefs.home);
+export async function load({ url }) {
+  const userId = url.searchParams.get("user_id");
+  if (!userId) throw redirect(301, hrefs.home);
+  const { data, error: supabaseError } = await supabase
+    .from("profiles")
+    .select("username")
+    .eq("user_id", userId);
+  if (supabaseError) throw error(400, "An error has occured");
+  throw redirect(301, hrefs.profile.replace("slug", data[0].username));
 }
