80023c94c98bce5290af06fb156b773e97f4f267
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-30 13:42:45 +0200

    Made fullscreen modal work

--- src/lib/components/FullscreenModal.svelte
+++ src/lib/components/FullscreenModal.svelte
@@ -1,13 +1,19 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import FloatElement from "./FloatElement.svelte";
   export let visible = true;
+  const dispatch = createEventDispatcher();
 </script>
 
 {#if visible}
-  <div class="z-10 min-h-screen w-screen absolute top-0 bg-gray-700 h-full">
+  <div
+    class="z-10 min-h-screen w-screen max-w-full overflow-auto absolute top-0 bg-gray-700 h-full"
+  >
     <slot />
   </div>
   <FloatElement>
-    <button on:click class="btn btn-neutral">Exit</button>
+    <button on:click={() => dispatch("exit")} class="btn btn-neutral"
+      >Exit</button
+    >
   </FloatElement>
 {/if}
