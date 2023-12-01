cf1139b5fc97b2ff8913c33465eaac522e1b1555
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-12-01 14:10:20 +0200

    Minor change

--- src/lib/components/ShareModal.svelte
+++ src/lib/components/ShareModal.svelte
@@ -3,16 +3,22 @@
   export let id = "share";
   export let title = "Share";
   export let link = "";
+
+  let copyText = link;
 </script>
 
 <Modal {id} {title}>
   <div class="join w-full">
     <button
       class="btn btn-accent rounded-r-none"
-      on:click={() => navigator.clipboard.writeText(link)}>Copy Link</button
+      on:click={() => {
+        navigator.clipboard.writeText(link);
+        copyText = "Link copied to clipboard";
+        setTimeout(() => (copyText = link), 3000);
+      }}>Copy Link</button
     >
     <input
-      value={link}
+      value={copyText}
       type="text"
       class="input w-full rounded-l-none disabled:cursor-text"
       disabled
