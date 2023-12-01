17b374124c8d7d893418b8aec9a16fb09efffe08
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-12-01 12:37:58 +0200

    Removed margin top

--- src/lib/components/FloatElement.svelte
+++ src/lib/components/FloatElement.svelte
@@ -3,9 +3,7 @@
 </script>
 
 {#if visible}
-  <div class=" mt-10 md:mt-0">
-    <div class="mx-5 mb-5 print:invisible z-50 bottom-0 right-0 fixed">
-      <slot />
-    </div>
+  <div class="mx-5 mb-5 print:invisible z-50 bottom-0 right-0 fixed">
+    <slot />
   </div>
 {/if}
