8b01bd6bc5ed4874b03347ed5a5ed795a100946d
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-12-01 12:19:37 +0200

    Added error message

--- src/routes/users/[slug]/birthday/+page.server.js
+++ src/routes/users/[slug]/birthday/+page.server.js
@@ -1,24 +1,31 @@
 import { NASA } from "$env/static/private";
 import { hrefs, supabase } from "$lib/index.js";
 import { error, redirect } from "@sveltejs/kit";
-import { addParamsString, formatDate } from "../../../../hooks.client.js";
+import { addParamsString } from "../../../../hooks.client.js";
 
 const minDate = "1995-06-16";
 const apiUrl = "https://api.nasa.gov/planetary/apod";
+
+function findMinDate(birthday = "") {
+  const date = "1995-06-" + birthday.slice(-2);
+  if (date >= minDate) return date;
+  return date.replace("1995", "1996");
+}
 
 export async function load({ params }) {
   const { data } = await supabase
     .from("profiles")
     .select("birthday")
     .eq("username", params.slug);
-  const birthday = data[0].birthday;
-  if (birthday < minDate)
-    throw error(400, {
-      message: `APOD is only available since ${formatDate(new Date(minDate))}`,
-    });
+  if (data.length == 0) throw error(404, "User not found");
+  let birthday = data[0].birthday;
+  if (!birthday) throw error(400, "User has not set a birthday");
+  if (birthday < minDate) birthday = findMinDate(birthday);
   const response = await fetch(
     addParamsString(apiUrl, { date: birthday, api_key: NASA })
   );
+  if (!response.ok)
+    throw error(response.status, { message: response.statusText });
   const item = await response.json();
   throw redirect(301, addParamsString(hrefs.viewer, item));
 }
