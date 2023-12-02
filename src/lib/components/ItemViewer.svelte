c9eb06f4a610d1de0c7b4a9da78655812a03112b
Oded2 <obenamotz@gmail.com>
2023-12-02 19:01:56 +0200

    Added padding

--- src/lib/components/ItemViewer.svelte
+++ src/lib/components/ItemViewer.svelte
@@ -20,7 +20,7 @@
   }
 </script>
 
-<div class="hero min-h-screen overflow-auto">
+<div class="hero min-h-screen overflow-auto py-5">
   <div class="hero-content flex-col xl:flex-row">
     <div class="text-center xl:text-left">
       <h1 class="text-5xl font-bold pb-2">{item.title}</h1>
