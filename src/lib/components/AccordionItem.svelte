9c29616bbda40deb18f707ca771c5c653f3e6eba
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-12-04 15:39:05 +0200

    Made header bold

--- src/lib/components/AccordionItem.svelte
+++ src/lib/components/AccordionItem.svelte
@@ -1,17 +1,16 @@
 <script>
-    export let header = "";
-    export let checked = false;
-    export let name =  "accordion"
-	export let border = false;
-
+  export let header = "";
+  export let checked = false;
+  export let name = "accordion";
+  export let border = false;
 </script>
 
 <div class="collapse collapse-plus bg-gray-900 mb-1" class:border>
-	<input type="radio" {name} {checked} />
-	<div class="collapse-title text-xl font-medium">
-		{header}
-	</div>
-	<div class="collapse-content">
-		<slot />
-	</div>
+  <input type="radio" {name} {checked} />
+  <div class="collapse-title text-xl font-bold">
+    {header}
+  </div>
+  <div class="collapse-content">
+    <slot />
+  </div>
 </div>
